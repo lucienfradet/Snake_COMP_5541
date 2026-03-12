35b28f55391813d2aff1c78831d74dbbddb8e76f
lucienfradet <contact@lucienfradet.com>
2026-03-12 15:49:17 -0400

    proper location for Main

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -8,16 +8,14 @@ public class Main {
       System.setProperty("sun.java2d.opengl", "true");
     }
 
-		//Creating the window with all its awesome snaky features
-		// Window window= new Window();
+		// Creating the window with all its awesome snaky features
+		Window window= new Window();
 		
-		//Setting up the window settings
-		// window.setTitle("Snake");
-		// window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		// window.pack();	//Fit window sizet to preferred JPanel sizes
-		// window.setLocationRelativeTo(null);
-		// window.setVisible(true);             
-
-    UserDB.createDatabase();
+		// Setting up the window settings
+		window.setTitle("Snake");
+		window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		window.pack();	//Fit window sizet to preferred JPanel sizes
+		window.setLocationRelativeTo(null);
+		window.setVisible(true);             
 	}
 }
