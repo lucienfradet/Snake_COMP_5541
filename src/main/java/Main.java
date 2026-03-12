a94053fa898f5cae6c6bd61457ea02992ccf89dc
lucienfradet <contact@lucienfradet.com>
2026-03-12 16:08:18 -0400

    small updates

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -9,16 +9,16 @@ public class Main {
     }
 
 		// Creating the window with all its awesome snaky features
-		// Window window= new Window();
+		Window window= new Window();
 		
 		// Setting up the window settings
-		// window.setTitle("Snake");
-		// window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		// window.pack();	//Fit window sizet to preferred JPanel sizes
-		// window.setLocationRelativeTo(null);
-		// window.setVisible(true);             
+		window.setTitle("Snake");
+		window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		window.pack();	//Fit window sizet to preferred JPanel sizes
+		window.setLocationRelativeTo(null);
+		window.setVisible(true);             
 
 
-    UserDB.createDatabase();
+    // UserDB.createDatabase();
 	}
 }
