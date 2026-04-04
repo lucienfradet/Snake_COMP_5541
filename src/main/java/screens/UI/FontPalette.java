1b3471a6386770ab04d123a5114366a2c8ef7f53
jinski6146 <zijin6146@icloud.com>
2026-04-04 18:49:54 -0400

    Update

--- src/main/java/screens/UI/FontPalette.java
+++ src/main/java/screens/UI/FontPalette.java
@@ -4,9 +4,10 @@ import java.awt.Font;
 
 public class FontPalette {
 
-    public static final Font TITLE = new Font("Yoster Island", Font.BOLD, 48);
-    public static final Font TEXT = new Font("Yoster Island", Font.BOLD, 20);
+    public static final String fontPath = "./data/yoster.ttf";
+    public static final Font TITLE = new Font(fontPath, Font.BOLD, 48);
+    public static final Font TEXT = new Font(fontPath, Font.BOLD, 20);
     
-    private FontPalette(){}
+    private FontPalette() {}
     
 }
--- src/test/java/UserDataTest.java
+++ src/test/java/UserDataTest.java
@@ -1,3 +1,5 @@
+import db.UserData;
+
 class UserDataTest {
 
     private UserData userData;
