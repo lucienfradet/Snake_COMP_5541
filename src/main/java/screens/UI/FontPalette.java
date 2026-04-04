2c2ee7b77f9a3bed1e85acd5120d6cf3b9b27962
lucienfradet <contact@lucienfradet.com>
2026-04-04 19:02:07 -0400

    fixed the font call

--- src/main/java/screens/UI/FontPalette.java
+++ src/main/java/screens/UI/FontPalette.java
@@ -1,13 +1,25 @@
 package screens.UI;
 
 import java.awt.Font;
+import java.awt.FontFormatException;
+import java.io.File;
+import java.io.IOException;
 
 public class FontPalette {
+    public static final String fontPath = "data/yoster.ttf";
 
-    public static final String fontPath = "./data/yoster.ttf";
-    public static final Font TITLE = new Font(fontPath, Font.BOLD, 48);
-    public static final Font TEXT = new Font(fontPath, Font.BOLD, 20);
-    
+    public static final Font TITLE = loadFont(48);
+    public static final Font TEXT  = loadFont(20);
+
+    private static Font loadFont(float size) {
+        try {
+            Font base = Font.createFont(Font.TRUETYPE_FONT, new File(fontPath));
+            return base.deriveFont(Font.BOLD, size);
+        } catch (FontFormatException | IOException e) {
+            e.printStackTrace();
+            return new Font("SansSerif", Font.BOLD, (int) size); // fallback
+        }
+    }
+
     private FontPalette() {}
-    
 }
