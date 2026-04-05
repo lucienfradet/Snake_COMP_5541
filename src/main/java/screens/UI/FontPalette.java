0132123680b57aea0552a1649ecc659bd3122376
jinski6146 <zijin6146@icloud.com>
2026-04-05 12:41:17 -0400

    Fixing font path

--- src/main/java/screens/UI/FontPalette.java
+++ src/main/java/screens/UI/FontPalette.java
@@ -4,16 +4,28 @@ import java.awt.Font;
 import java.awt.FontFormatException;
 import java.io.File;
 import java.io.IOException;
+import java.io.InputStream;
 
 public class FontPalette {
-    public static final String fontPath = "data/yoster.ttf";
+    
+    public static final String FONT_RESOURCE_PATH = "/data/yoster.ttf";
+    public static final String FONT_FILE_PATH = "data/yoster.ttf";
 
     public static final Font TITLE = loadFont(48);
     public static final Font TEXT  = loadFont(20);
 
     private static Font loadFont(float size) {
+        try (InputStream fontStream = FontPalette.class.getResourceAsStream(FONT_RESOURCE_PATH)) {
+            if (fontStream != null) {
+                Font base = Font.createFont(Font.TRUETYPE_FONT, fontStream);
+                return base.deriveFont(Font.BOLD, size);
+            }
+        } catch (FontFormatException | IOException e) {
+            e.printStackTrace();
+        }
+
         try {
-            Font base = Font.createFont(Font.TRUETYPE_FONT, new File(fontPath));
+            Font base = Font.createFont(Font.TRUETYPE_FONT, new File(FONT_FILE_PATH));
             return base.deriveFont(Font.BOLD, size);
         } catch (FontFormatException | IOException e) {
             e.printStackTrace();
